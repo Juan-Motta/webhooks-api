7b5901ae3cd43189d36a4ca0951ec0165cb3c331
Juan Andres Lopez Motta <juan.lopez@smartquick.com.co>
2023-01-15 23:56:21 -0500

    Added typeorm config

---
+++ src/db.ts
@@ -0,0 +1,18 @@
+import { DataSource } from "typeorm";
+
+import { config } from "./config";
+
+const ServiceDataSource: DataSource = new DataSource({
+    type: 'postgres',
+    host: config.service_db.host,
+    username: config.service_db.user,
+    password: config.service_db.password,
+    port: config.service_db.port,
+    database: config.service_db.database,
+    entities: []
+
+})
+
+export {
+    ServiceDataSource
+}
