c29d3d30d91a7440666ddf8d856e6852323db823
Juan Andres Lopez Motta <juan.lopez@smartquick.com.co>
2023-02-05 00:06:45 -0500

    Removed typeorm init

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 import 'reflect-metadata';
 
 import { config } from './config';
-import { ServiceDataSource } from './db';
 import { RabbitClient } from './amqp';
 import { app } from './app';
 
 async function main() {
 
-    await ServiceDataSource.initialize();
     await RabbitClient(`amqp://${config.rabbit.host}:${config.rabbit.port}`, config.rabbit.queue);
     
     app.listen( config.port ,() => {
